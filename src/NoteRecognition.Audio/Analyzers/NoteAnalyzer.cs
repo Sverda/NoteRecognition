dc9be05eceb120946dab94088c371a376cfef9c8
Damian Ubowski <sverda300@gmail.com>
2020-02-02 20:24:53 +0100

    Add unit test for notes without sharp

--- src/NoteRecognition.Audio/Analyzers/NoteAnalyzer.cs
+++ src/NoteRecognition.Audio/Analyzers/NoteAnalyzer.cs
@@ -1,7 +1,77 @@
-﻿namespace NoteRecognition.Audio.Analyzers
+﻿using System;
+
+namespace NoteRecognition.Audio.Analyzers
 {
     public class NoteAnalyzer
     {
+        public string CheckNote(double frequency)
+        {
+            if (frequency < 403.5)
+            {
+                return "G";
+            }
 
+            if (403.5 <= frequency && frequency < 427.5)
+            {
+                return "G#";
+            }
+
+            if (427.5 <= frequency && frequency < 451.5)
+            {
+                return "A";
+            }
+
+            if (451.5 <= frequency && frequency < 479.5)
+            {
+                return "A#";
+            }
+
+            if (479.5 <= frequency && frequency < 508)
+            {
+                return "B";
+            }
+
+            if (508 <= frequency && frequency < 538.5)
+            {
+                return "C";
+            }
+
+            if (538.5 <= frequency && frequency < 570.5)
+            {
+                return "C#";
+            }
+
+            if (570.5 <= frequency && frequency < 604.5)
+            {
+                return "D";
+            }
+
+            if (604.5 <= frequency && frequency < 640.5)
+            {
+                return "D#";
+            }
+
+            if (640.5 <= frequency && frequency < 678.5)
+            {
+                return "E";
+            }
+
+            if (678.5 <= frequency && frequency < 719)
+            {
+                return "F";
+            }
+
+            if (719 <= frequency && frequency < 762)
+            {
+                return "F#";
+            }
+
+            if (762 <= frequency)
+            {
+                return "G";
+            }
+
+            throw new ArgumentException("Unknown note");
+        }
     }
 }
--- src/NoteRecognition.Tests/NotesTests.cs
+++ src/NoteRecognition.Tests/NotesTests.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NoteRecognition.Audio.Analyzers;
 using NoteRecognition.Audio.Readers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace NoteRecognition.Tests
 {
@@ -10,7 +12,48 @@ namespace NoteRecognition.Tests
         [TestMethod]
         public void G()
         {
-            var filePath = @"C:\Users\Damian\source\repos\NoteRecognition\audio\G.wav";
+            TestCurrentNote();
+        }
+
+        [TestMethod]
+        public void A()
+        {
+            TestCurrentNote();
+        }
+
+        [TestMethod]
+        public void B()
+        {
+            TestCurrentNote();
+        }
+
+        [TestMethod]
+        public void C()
+        {
+            TestCurrentNote();
+        }
+
+        [TestMethod]
+        public void D()
+        {
+            TestCurrentNote();
+        }
+
+        [TestMethod]
+        public void E()
+        {
+            TestCurrentNote();
+        }
+
+        [TestMethod]
+        public void F()
+        {
+            TestCurrentNote();
+        }
+
+        private void TestCurrentNote()
+        {
+            var filePath = GetCurrentNotePath();
 
             var waveReader = new WaveDataFileReader(filePath);
             var audioAnalyzer = new AudioAnalyzer(waveReader)
@@ -20,7 +63,30 @@ namespace NoteRecognition.Tests
             audioAnalyzer.AnalyzeValues();
 
             var maxFrequency = audioAnalyzer.FindMaxAmplitude().Frequency;
+            var actualResult = new NoteAnalyzer().CheckNote(maxFrequency);
 
+            AssertNote(actualResult);
+        }
+
+        private string GetCurrentNotePath()
+        {
+            var noteName = GetCurrentTestName();
+            var filePath = $"..\\..\\..\\..\\audio\\{noteName}.wav";
+            return filePath;
+        }
+
+        private void AssertNote(string actualNote)
+        {
+            Assert.AreEqual(GetCurrentTestName(), actualNote);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private string GetCurrentTestName()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(3);
+
+            return sf.GetMethod().Name;
         }
     }
 }
