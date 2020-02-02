b0f46bf7d80565192c7de0596e4ffe4c2eb78e39
Damian Ubowski <sverda300@gmail.com>
2020-02-02 19:28:00 +0100

    Rename audio file & Add unit test project

---
+++ src/NoteRecognition.Tests/NotesTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoteRecognition.Audio.Analyzers;
+using NoteRecognition.Audio.Readers;
+
+namespace NoteRecognition.Tests
+{
+    [TestClass]
+    public class NotesTests
+    {
+        [TestMethod]
+        public void G()
+        {
+            var filePath = @"C:\Users\Damian\source\repos\NoteRecognition\audio\G.wav";
+
+            var waveReader = new WaveDataFileReader(filePath);
+            var audioAnalyzer = new AudioAnalyzer(waveReader)
+            {
+                FftLength = 1024 * 16
+            };
+            audioAnalyzer.AnalyzeValues();
+
+            var maxFrequency = audioAnalyzer.FindMaxAmplitude().Frequency;
+
+        }
+    }
+}
---
+++ src/NoteRecognition.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NoteRecognition.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("NoteRecognition.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc5c84aa-350e-486a-80b4-d474f3d5c5c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
